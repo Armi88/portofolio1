e9441a488a8e8a5df60912177f76c57f31cd3c26
Armi88 <varianarmi78@gmail.com>
2026-06-30 12:45:47 +0700

    feat: add CV download link in About section

--- src/App.jsx
+++ src/App.jsx
@@ -497,7 +497,7 @@ export default developer;`}</pre>
                 ))}
               </div>
               <a
-                href="#"
+                href="https://drive.google.com/file/d/1Jcq4M62vD2cupNL_MKRePw8tamih0LSf/view?usp=sharing"
                 className="cv-download-btn"
                 target="_blank"
                 rel="noopener noreferrer"
